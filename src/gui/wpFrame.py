17d7161f3671391b1345494a1ecc8f9807f3ca10
Roger C.B. Johnsen <johnsen@opendefinition.com>
2009-01-10 20:55:21 +0100

    Initial GUI classes and configuration folder

---
+++ src/gui/wpFrame.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+class wpFrame:
+    def __init__( self ):
+        None
---
+++ src/gui/wpNotebook.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+class  wpNoteBook:
+    def __init__( self ):
+        None
