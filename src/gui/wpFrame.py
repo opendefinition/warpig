21ff88d533cf1de3350ab81655857db09b679100
Roger C.B. Johnsen <johnsen@opendefinition.com>
2009-01-12 21:52:38 +0100

    Added conceptual editor component

--- src/gui/wpFrame.py
+++ src/gui/wpFrame.py
@@ -1,11 +1,52 @@
 # -*- coding: utf-8 -*-
 
 import wx
+import wx.stc as stc
 
 class wpFrame( wx.Frame ):
     def __init__( self, *args, **kwargs ):
         wx.Frame.__init__( self, *args, **kwargs )
-        self.create_controls()
+        self.setup_widgets()
         
-    def create_controls( self ):
-        pass
+    def setup_widgets( self ):
+        """Called when the controls on Window are to be created"""
+
+        #Horizontal sizer
+        self.h_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        #Vertical sizer
+        self.v_sizer = wx.BoxSizer(wx.VERTICAL)
+        
+        self.textEditor = stc.StyledTextCtrl ( self, -1, style=wx.TE_MULTILINE )
+        self.textEditor.SetMarginLeft( 10 )
+        self.textEditor.SetLexerLanguage( 'python' )
+        
+        
+        self.h_sizer.Add( self.textEditor, 0)
+        
+        self.v_sizer.Add(self.h_sizer, 0, wx.EXPAND)
+        
+        '''
+        #Widget Creation
+        #Create the static text widget and set the text
+        self.text = wx.StaticText(self, label="Enter some text:")
+        #Create the Edit Field (or TextCtrl)
+        self.edit = wx.TextCtrl(self, size=wx.Size(250, -1))
+        #Create the button
+        self.button = wx.Button(self, label="Press me!")
+    
+        #Add to horizontal sizer
+        #add the static text to the sizer, tell it not to resize
+        self.h_sizer.Add(self.text, 0,)
+        #Add 5 pixels between the static text and the edit
+        self.h_sizer.AddSpacer((5,0))
+        #Add Edit
+        self.h_sizer.Add(self.edit, 1)
+    
+        #Add to the vertical sizer to create two rows
+        self.v_sizer.Add(self.h_sizer, 0, wx.EXPAND)
+        #Add button underneath
+        self.v_sizer.Add(self.button, 0)
+    
+        #Set the sizer
+        self.SetSizer(self.v_sizer)
+        '''
--- src/gui/wpWarPig.py
+++ src/gui/wpWarPig.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import wx
 
+from gui.wpFrame import wpFrame
+
 class wpWarPig( wx.App ):
     """The wx.App for the wxHello application"""
 
     def OnInit(self):
         """Override OnInit to create our Frame"""
-        frame = wx.Frame( None, title='Something' )
+        frame = wpFrame( None, title='Something' )
         frame.Center()
         frame.Show()
         self.SetTopWindow(frame)
