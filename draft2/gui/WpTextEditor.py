f4ca6321897538b3a3060098d54e09476583eda5
Roger C.B. Johnsen <johnsen@opendefinition.com>
2009-10-09 22:52:21 +0200

    Experimental code folding

--- draft2/gui/WpTextEditor.py
+++ draft2/gui/WpTextEditor.py
@@ -14,6 +14,7 @@ import keyword
 import os
 import re 
 import wx
+import wx.stc as stc
 
 from system.WpFileSystem import WpFileSystem
 from system.WpConfigSystem import WpConfigSystem
@@ -46,6 +47,192 @@ class WpTextEditor( wx.stc.StyledTextCtrl ):
                 self.SetTabWidth(self.editorTabSize)
                 self.SetUseTabs(False)
 
+        def setCodeFolding(self):
+                """
+                Setup code folding
+                """
+                self.fold_symbols = 2
+                self.SetProperty("fold", "1") ## What is this???
+                self.SetMarginType(2, stc.STC_MARGIN_SYMBOL)
+                self.SetMarginMask(2, stc.STC_MASK_FOLDERS)
+                
+                self.SetMarginSensitive(2, True)
+                self.SetMarginWidth(2, 12)
+                self.SetFoldMarginHiColour(True, "#99cc00")
+                self.SetFoldMarginColour(True, "336699")
+
+
+                if self.fold_symbols == 0:
+                    # Arrow pointing right for contracted folders, arrow pointing down for expanded
+                    self.MarkerDefine( stc.STC_MARKNUM_FOLDEROPEN,    stc.STC_MARK_ARROWDOWN,    "black", "black" )
+                    self.MarkerDefine( stc.STC_MARKNUM_FOLDER,        stc.STC_MARK_ARROW,        "black", "black" )
+                    self.MarkerDefine( stc.STC_MARKNUM_FOLDERSUB,     stc.STC_MARK_EMPTY,        "black", "black" )
+                    self.MarkerDefine( stc.STC_MARKNUM_FOLDERTAIL,    stc.STC_MARK_EMPTY,        "black", "black" )
+                    self.MarkerDefine( stc.STC_MARKNUM_FOLDEREND,     stc.STC_MARK_EMPTY,        "white", "black" )
+                    self.MarkerDefine( stc.STC_MARKNUM_FOLDEROPENMID, stc.STC_MARK_EMPTY,        "white", "black" )
+                    self.MarkerDefine( stc.STC_MARKNUM_FOLDERMIDTAIL, stc.STC_MARK_EMPTY,        "white", "black" )
+                elif self.fold_symbols == 1:
+                    # Plus for contracted folders, minus for expanded
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDEROPEN,    stc.STC_MARK_MINUS, "white", "black")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDER,        stc.STC_MARK_PLUS,  "white", "black")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDERSUB,     stc.STC_MARK_EMPTY, "white", "black")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDERTAIL,    stc.STC_MARK_EMPTY, "white", "black")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDEREND,     stc.STC_MARK_EMPTY, "white", "black")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDEROPENMID, stc.STC_MARK_EMPTY, "white", "black")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDERMIDTAIL, stc.STC_MARK_EMPTY, "white", "black")
+
+                elif self.fold_symbols == 2:
+                    # Like a flattened tree control using circular headers and curved joins
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDEROPEN,    stc.STC_MARK_CIRCLEMINUS,          "white", "#404040")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDER,        stc.STC_MARK_CIRCLEPLUS,           "white", "#404040")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDERSUB,     stc.STC_MARK_VLINE,                "white", "#404040")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDERTAIL,    stc.STC_MARK_LCORNERCURVE,         "white", "#404040")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDEREND,     stc.STC_MARK_CIRCLEPLUSCONNECTED,  "white", "#404040")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDEROPENMID, stc.STC_MARK_CIRCLEMINUSCONNECTED, "white", "#404040")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDERMIDTAIL, stc.STC_MARK_TCORNERCURVE,         "white", "#404040")
+
+                elif self.fold_symbols == 3:
+                    # Like a flattened tree control using square headers
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDEROPEN,    stc.STC_MARK_BOXMINUS,          "white", "#808080")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDER,        stc.STC_MARK_BOXPLUS,           "white", "#808080")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDERSUB,     stc.STC_MARK_VLINE,             "white", "#808080")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDERTAIL,    stc.STC_MARK_LCORNER,           "white", "#808080")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDEREND,     stc.STC_MARK_BOXPLUSCONNECTED,  "white", "#808080")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDEROPENMID, stc.STC_MARK_BOXMINUSCONNECTED, "white", "#808080")
+                    self.MarkerDefine(stc.STC_MARKNUM_FOLDERMIDTAIL, stc.STC_MARK_TCORNER,           "white", "#808080")
+
+                self.Bind(stc.EVT_STC_UPDATEUI, self.OnUpdateUI)
+                self.Bind(stc.EVT_STC_MARGINCLICK, self.OnMarginClick)
+        
+        def OnUpdateUI(self, evt):
+            # check for matching braces
+            braceAtCaret = -1
+            braceOpposite = -1
+            charBefore = None
+            caretPos = self.GetCurrentPos()
+
+            if caretPos > 0:
+                charBefore = self.GetCharAt(caretPos - 1)
+                styleBefore = self.GetStyleAt(caretPos - 1)
+
+            # check before
+            if charBefore and chr(charBefore) in "[]{}()" and styleBefore == stc.STC_P_OPERATOR:
+                braceAtCaret = caretPos - 1
+
+            # check after
+            if braceAtCaret < 0:
+                charAfter = self.GetCharAt(caretPos)
+                styleAfter = self.GetStyleAt(caretPos)
+
+                if charAfter and chr(charAfter) in "[]{}()" and styleAfter == stc.STC_P_OPERATOR:
+                    braceAtCaret = caretPos
+
+            if braceAtCaret >= 0:
+                braceOpposite = self.BraceMatch(braceAtCaret)
+
+            if braceAtCaret != -1  and braceOpposite == -1:
+                self.BraceBadLight(braceAtCaret)
+            else:
+                self.BraceHighlight(braceAtCaret, braceOpposite)
+                #pt = self.PointFromPosition(braceOpposite)
+                #self.Refresh(True, wxRect(pt.x, pt.y, 5,5))
+                #print pt
+                #self.Refresh(False)
+
+
+        def OnMarginClick(self, evt):
+            # fold and unfold as needed
+            if evt.GetMargin() == 2:
+                if evt.GetShift() and evt.GetControl():
+                    self.FoldAll()
+                else:
+                    lineClicked = self.LineFromPosition(evt.GetPosition())
+
+                    if self.GetFoldLevel(lineClicked) & stc.STC_FOLDLEVELHEADERFLAG:
+                        if evt.GetShift():
+                            self.SetFoldExpanded(lineClicked, True)
+                            self.Expand(lineClicked, True, True, 1)
+                        elif evt.GetControl():
+                            if self.GetFoldExpanded(lineClicked):
+                                self.SetFoldExpanded(lineClicked, False)
+                                self.Expand(lineClicked, False, True, 0)
+                            else:
+                                self.SetFoldExpanded(lineClicked, True)
+                                self.Expand(lineClicked, True, True, 100)
+                        else:
+                            self.ToggleFold(lineClicked)
+
+
+        def FoldAll(self):
+            lineCount = self.GetLineCount()
+            expanding = True
+
+            # find out if we are folding or unfolding
+            for lineNum in range(lineCount):
+                if self.GetFoldLevel(lineNum) & stc.STC_FOLDLEVELHEADERFLAG:
+                    expanding = not self.GetFoldExpanded(lineNum)
+                    break
+
+            lineNum = 0
+
+            while lineNum < lineCount:
+                level = self.GetFoldLevel(lineNum)
+                if level & stc.STC_FOLDLEVELHEADERFLAG and \
+                   (level & stc.STC_FOLDLEVELNUMBERMASK) == stc.STC_FOLDLEVELBASE:
+
+                    if expanding:
+                        self.SetFoldExpanded(lineNum, True)
+                        lineNum = self.Expand(lineNum, True)
+                        lineNum = lineNum - 1
+                    else:
+                        lastChild = self.GetLastChild(lineNum, -1)
+                        self.SetFoldExpanded(lineNum, False)
+
+                        if lastChild > lineNum:
+                            self.HideLines(lineNum+1, lastChild)
+
+                lineNum = lineNum + 1
+
+
+
+        def Expand(self, line, doExpand, force=False, visLevels=0, level=-1):
+            lastChild = self.GetLastChild(line, level)
+            line = line + 1
+
+            while line <= lastChild:
+                if force:
+                    if visLevels > 0:
+                        self.ShowLines(line, line)
+                    else:
+                        self.HideLines(line, line)
+                else:
+                    if doExpand:
+                        self.ShowLines(line, line)
+
+                if level == -1:
+                    level = self.GetFoldLevel(line)
+
+                if level & stc.STC_FOLDLEVELHEADERFLAG:
+                    if force:
+                        if visLevels > 1:
+                            self.SetFoldExpanded(line, True)
+                        else:
+                            self.SetFoldExpanded(line, False)
+
+                        line = self.Expand(line, doExpand, force, visLevels-1)
+
+                    else:
+                        if doExpand and self.GetFoldExpanded(line):
+                            line = self.Expand(line, True, force, visLevels-1)
+                        else:
+                            line = self.Expand(line, False, force, visLevels-1)
+                else:
+                    line = line + 1
+
+            return line
+
+
+
 	def __init__( self, parent ):
 		self.parent = parent
 
@@ -66,7 +253,10 @@ class WpTextEditor( wx.stc.StyledTextCtrl ):
 		
 		## Tab Setup
 		self.setTabAndIndents()
-		
+		## Code folding
+                self.setCodeFolding()
+
+
 		## Fonts	
 		font = wx.Font(
                             self.editorFontSize,
@@ -80,32 +270,6 @@ class WpTextEditor( wx.stc.StyledTextCtrl ):
 	
 		## We must tell StyleSetSpec to use this face and size otherwise it won't get applied
 		self.StyleSetSpec(wx.stc.STC_STYLE_DEFAULT,"face:%s,size:%d" % (self.editorFontFace, self.editorFontSize))
-		
-		##
-		# Code folding
-		# see: http://www.python-forum.org/pythonforum/viewtopic.php?f=2&t=10065
-		##
-		"""
-		self.SetProperty("fold", "1")
-		self.SetMargins(0, 0)
-		self.SetViewWhiteSpace(False)
-		self.SetEdgeMode(wx.stc.STC_EDGE_BACKGROUND)
-		self.SetEdgeColumn(78)
-		
-		# setup a margin to hold the fold markers
-		self.SetMarginType(2, wx.stc.STC_MARGIN_SYMBOL)
-		self.SetMarginMask(2, wx.stc.STC_MASK_FOLDERS)
-		self.SetMarginSensitive(2, True)
-		self.SetMarginWidth(2, 12)
-		
-		self.MarkerDefine( wx.stc.STC_MARKNUM_FOLDEROPEN, wx.stc.STC_MARK_BOXMINUS, "white", "#808080")
-		self.MarkerDefine( wx.stc.STC_MARKNUM_FOLDER, wx.stc.STC_MARK_BOXPLUS, "white", "#808080")
-		self.MarkerDefine( wx.stc.STC_MARKNUM_FOLDERSUB, wx.stc.STC_MARK_VLINE, "white", "#808080")
-		self.MarkerDefine( wx.stc.STC_MARKNUM_FOLDERTAIL, wx.stc.STC_MARK_LCORNER, "white", "#808080")
-		self.MarkerDefine( wx.stc.STC_MARKNUM_FOLDEREND, wx.stc.STC_MARK_BOXPLUSCONNECTED, "white", "#808080")
-		self.MarkerDefine( wx.stc.STC_MARKNUM_FOLDEROPENMID, wx.stc.STC_MARK_BOXMINUSCONNECTED, "white", "#808080")
-		self.MarkerDefine( wx.stc.STC_MARKNUM_FOLDERMIDTAIL, wx.stc.STC_MARK_TCORNER, "white", "#808080")	
-		"""
 		
 		self.StyleSetFont( 0, font )
 		self.SetDefaultLexer()
@@ -174,21 +338,7 @@ class WpTextEditor( wx.stc.StyledTextCtrl ):
 		self.StyleSetSpec( wx.stc.STC_STYLE_BRACELIGHT, "fore:#00FF00,back:#232323,bold" )
 		self.StyleSetSpec( wx.stc.STC_STYLE_BRACEBAD, "fore:#00FF00,back:#232323,bold" )
 		
-		##
-		# Python styles
-		##
-		
-		"""
-		bold                    turns on bold
-		italic                  turns on italics
-		fore:[name or #RRGGBB]  sets the foreground colour
-		back:[name or #RRGGBB]  sets the background colour
-		face:[facename]         sets the font face name to use
-		size:[num]              sets the font size in points
-		eol                     turns on eol filling
-		underline               turns on underlining
-
-		"""
+		## Python styles
 		
 		##
 		# Default
