cbea49a2c163596543bbafeed764c789017ea037
Roger C.B. Johnsen <johnsen@opendefinition.com>
2009-10-17 14:39:47 +0200

    Draft for new project handler

--- source/db/sql/default-system.sql
+++ source/db/sql/default-system.sql
@@ -1,17 +1,33 @@
--- System Registry
+;-- System Registry
 CREATE TABLE systemregistry(
 	"key" VARCHAR(50) PRIMARY KEY NOT NULL,
 	"value" VARCHAR(50) DEFAULT NULL,
 	"module" VARCHAR(50)  DEFAULT NULL
 );
 
--- System information table
+;-- System information table
 CREATE TABLE systeminformation(
 	"key" VARCHAR(50) PRIMARY KEY NOT NULL,
 	"value" VARCHAR(50) DEFAULT NULL
 );
 
--- DEFAULT EDITOR VALUES
+;-- Main project table
+CREATE TABLE projects(
+    "id" INTEGER PRIMARY KEY,
+    "title" VARCHAR(100) NOT NULL,
+    "datecreated" DATETIME DEFAULT current_timestamp
+);
+
+;-- Associated project files
+CREATE TABLE projectincludes(
+    "id" INTEGER,
+    "projectid" INTEGER NOT NULL,
+    "path" VARCHAR(255) NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+
+;-- DEFAULT EDITOR VALUES
 INSERT INTO systemregistry(key,value,module) VALUES ('fontface','Verdana','editor');
 INSERT INTO systemregistry(key,value,module) VALUES ('fontsize','12','editor');
 INSERT INTO systemregistry(key,value,module) VALUES ('textmargin','200','editor');
