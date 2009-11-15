9923230ef7b9bc1c76bcac2064857b514fe94a6b
Roger C.B. Johnsen <johnsen@opendefinition.com>
2009-11-15 08:20:02 +0100

    Moved the main menu into a class

--- source/gui/WpMainFrame.py
+++ source/gui/WpMainFrame.py
@@ -14,10 +14,11 @@
 
 import wx
 import wx.stc as stc
+
 from gui.WpMainPanel import WpMainPanel
-from gui.WpNewProject import WpNewProject
-from gui.WpOpenProject import WpOpenProject
-from gui.preferences.WpPreferences import WpPreferences
+
+# New main menu
+from gui.WpMainMenu import WpMainMenu
 
 from system.WpFileSystem import WpFileSystem
 
@@ -44,10 +45,8 @@ class WpMainFrame( wx.Frame ):
 		##
 		self.mainpanel = WpMainPanel( self )
 		
-		##
-		# Menubar
-		##
-		self.SetMenuBar( self._SetupMenuBar() )
+		## Menubar
+		self.SetMenuBar(WpMainMenu(self))
 		
 		##
 		# Adding it all to the mix
@@ -71,82 +70,6 @@ class WpMainFrame( wx.Frame ):
 		self.Maximize()
 		self.Show( True )
 		
-		
-	#---------------------------------------------------------------
-	# Create and setup menubar
-	# @return object menubar
-	#---------------------------------------------------------------
-	def _SetupMenuBar( self ):
-		self.menubar = wx.MenuBar()
-		
-		file = wx.Menu()
-		edit = wx.Menu()
-		view = wx.Menu()
-		piglets = wx.Menu()
-		help = wx.Menu()
-		
-		self.menubar.Append( file, '&File' )
-		self.menubar.Append( edit, '&Edit' )
-		self.menubar.Append( view, '&View' )
-		self.menubar.Append( piglets, '&Piglets' )
-		self.menubar.Append( help, '&Help' )
-		
-		##
-		# File menu entries
-		##
-		newproject = wx.MenuItem( file, 10101, '&New Project', 'Create new project' )
-		openproject = wx.MenuItem( file, 10102, '&Open Project', 'Open project' )
-		openfile = wx.MenuItem( file, 10103, '&Open File', 'Open file' )
-		newfile = wx.MenuItem( file, 10104, '&New File', 'Create empty document' )
-		savefile = wx.MenuItem( file, 10105, '&Save File', 'Save current file' )
-		savefileas = wx.MenuItem( file, 10106, '&Save File As', 'Save current file as...' )
-		exit = wx.MenuItem( file, 10107, '&Exit', 'Exit' )
-	
-		file.AppendItem( newproject )
-		file.AppendItem( openproject )
-		file.AppendSeparator()
-		file.AppendItem( newfile )
-		file.AppendItem( openfile )
-		file.AppendItem( savefile )
-		file.AppendItem( savefileas )
-		file.AppendSeparator()
-		file.AppendItem( exit )
-		
-		##
-		# Edit menu entries
-		##
-		preferences = wx.MenuItem( edit, 20210, '&Preferences', 'Edit application preferences' )
-		
-		edit.AppendSeparator()
-		edit.AppendItem( preferences )
-		
-		##
-		# Help menu entries
-		##
-		
-		helphelp = wx.MenuItem( help, 10501, '&Help', 'Get help' )
-		helprequestfeature = wx.MenuItem( help, 10502, 'Request feature', 'Request feature' )
-		helpreportbug = wx.MenuItem( help, 10503, 'Report bug', 'Submit bug' )
-	 	helpabout = wx.MenuItem( help, 10510, '&About', 'About WarPig'  )
-		
-		help.AppendItem( helphelp )
-		help.AppendSeparator()
-		help.AppendItem( helprequestfeature )
-		help.AppendItem( helpreportbug ) 
-		help.AppendSeparator()
-		help.AppendItem( helpabout )
-		
-		self.Bind( wx.EVT_MENU, self._OnAbout, id=10510 )
-		self.Bind( wx.EVT_MENU, self._OnExit, id=10107 )
-		self.Bind( wx.EVT_MENU, self._OnNewProject, id=10101 )
-		self.Bind( wx.EVT_MENU, self._OnOpenProject, id=10102 )
-		self.Bind( wx.EVT_MENU, self.mainpanel._OnToolBarOpenPage, id=10103 )
-		self.Bind( wx.EVT_MENU, self.mainpanel._OnToolBarNewPage, id=10104 )
-		self.Bind( wx.EVT_MENU, self.mainpanel._OnToolBarSavePage, id=10105 )
-		self.Bind( wx.EVT_MENU, self._OnPreferences, id=20210 )
-		
-		return self.menubar
-		
 	#---------------------------------------------------------------
 	# Create and setup statusbar
 	# @return object statusbar
@@ -155,59 +78,4 @@ class WpMainFrame( wx.Frame ):
 		self.statusbar = wx.StatusBar( self, -1 )
 		
 		return self.statusbar
-		
-	#---------------------------------------------------------------
-	# Bindings Section
-	#---------------------------------------------------------------
-	
-	#---------------------------------------------------------------
-	# Populates an about dialog and dispays it
-	#---------------------------------------------------------------
-	def _OnAbout( self, event ):
-		information = wx.AboutDialogInfo()
- 		information.SetName( 'Open Definition :: Warpig Code Environment' )
- 		information.SetVersion( '0.02 - Alpha Public' )
-  		information.SetDescription( 'A simple but yet powerfull code evironment written in Python.' )
-  		information.SetCopyright('Open Definition(C)2009' )
-  		information.SetWebSite( 'http://www.opendefinition.com' )
-  		information.AddDeveloper( 'Roger C.B. Johnsen' )
-  		
-  		wx.AboutBox( information )
-  	
-	#---------------------------------------------------------------
-	# Handle 'on exit' event
-	#---------------------------------------------------------------
-  	def _OnExit( self, event ):
-		dialog = wx.MessageDialog( None, 
-									'Are you sure to want to quit?', 
-									'Question',
-									wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION 
-								)
-		
-		status = dialog.ShowModal()
-	
-		if( status == wx.ID_YES ):
-			dialog.Destroy()
-			self.Close()
-		else:
-			dialog.Destroy()
-  		
-	#---------------------------------------------------------------
-	# Handle 'on new project' event
-	#---------------------------------------------------------------
-  	def _OnNewProject( self, event ):
-  		window = WpNewProject( self.mainpanel.leftsplit )
-		window.ShowModal()
-		window.Destroy()
-	
-	#---------------------------------------------------------------
-	# Handle 'on open project' event
-	#---------------------------------------------------------------
-	def _OnOpenProject( self, event ):
-                window = WpOpenProject(self.mainpanel.leftsplit)
-                window.ShowModal()
-
-	def _OnPreferences( self, event ):
-		preferences = WpPreferences()
-		preferences.ShowModal()
 		
---
+++ source/gui/WpMainMenu.py
@@ -0,0 +1,177 @@
+## -*- coding: utf-8 -*
+##---------------------------------------------------------------------------
+##
+## Class: WpMainMenu
+## Desc: Class for managing the main menu of this application
+##
+##---------------------------------------------------------------------------
+##
+## Owner: Open Definition.
+## Author: Roger C.B. Johnsen.
+## License: Open Definiton General Lisence (ODGL). Available upon request.
+##
+##---------------------------------------------------------------------------
+
+import wx
+
+from gui.WpNewProject import WpNewProject
+from gui.WpOpenProject import WpOpenProject
+from gui.preferences.WpPreferences import WpPreferences
+
+class WpMainMenu(wx.MenuBar):
+    def __init__(self, parentFrame):
+        wx.MenuBar.__init__(self)
+        self.parentFrame = parentFrame
+        self.Append(self.__setupFileMenu(), '&File')
+        self.Append(self.__setupEditMenu(), '&Edit')
+        self.Append(self.__setupEditMenu(), '&Edit')
+	self.Append(self.__setupViewMenu(), '&View')
+	self.Append(self.__setupPigletMenu(), '&Piglets')
+	self.Append(self.__setupHelpMenu(), '&Help')
+
+    ##--------------------------------------------------------------------------
+    ## Menu structures
+    ##--------------------------------------------------------------------------
+
+    def __setupFileMenu(self):
+        fileMenu = wx.Menu()
+
+        openProject = wx.MenuItem(fileMenu, wx.ID_ANY, '&Open Project', 'Open project')
+        newProject  = wx.MenuItem(fileMenu, wx.ID_ANY, '&New Project', 'Create new project')
+	saveFileAs  = wx.MenuItem(fileMenu, wx.ID_ANY, '&Save File As', 'Save current file as...')
+        openFile    = wx.MenuItem(fileMenu, wx.ID_ANY, '&Open File', 'Open file')
+        saveFile    = wx.MenuItem(fileMenu, wx.ID_ANY, '&Save File', 'Save current file')
+        newFile     = wx.MenuItem(fileMenu, wx.ID_ANY, '&New File', 'Create empty document')
+	exit        = wx.MenuItem(fileMenu, wx.ID_ANY, '&Exit', 'Exit')
+
+        fileMenu.AppendItem(newProject)
+        fileMenu.AppendItem(openProject)
+        fileMenu.AppendSeparator()
+        fileMenu.AppendItem(newFile)
+        fileMenu.AppendItem(openFile)
+        fileMenu.AppendItem(saveFile)
+        fileMenu.AppendItem(saveFileAs)
+        fileMenu.AppendSeparator()
+        fileMenu.AppendItem(exit)
+
+        self.parentFrame.Bind(wx.EVT_MENU, self.__onExit, id=exit.GetId())
+	self.parentFrame.Bind(wx.EVT_MENU, self.__onNewProject, id=newProject.GetId())
+	self.parentFrame.Bind(wx.EVT_MENU, self.__onOpenProject, id=openProject.GetId())
+
+        ## @TODO: Fix this mangling of references
+        self.parentFrame.Bind(wx.EVT_MENU, self.parentFrame.mainpanel._OnToolBarOpenPage, id=openFile.GetId())
+	self.parentFrame.Bind(wx.EVT_MENU, self.parentFrame.mainpanel._OnToolBarNewPage, id=newFile.GetId())
+	self.parentFrame.Bind(wx.EVT_MENU, self.parentFrame.mainpanel._OnToolBarSavePage, id=saveFile.GetId())
+
+        return fileMenu
+
+    def __setupEditMenu(self):
+        """
+        Setup the edit menu
+        """
+        editMenu = wx.Menu()
+
+        preferences = wx.MenuItem(editMenu, wx.ID_ANY, '&Preferences', 'Edit application preferences')
+
+        self.parentFrame.Bind(wx.EVT_MENU, self.__onPreferences, id=preferences.GetId())
+
+	editMenu.AppendSeparator()
+	editMenu.AppendItem(preferences)
+
+        return editMenu
+
+    def __setupViewMenu(self):
+        """
+        Setup the view menu
+        """
+        viewMenu = wx.Menu()
+        return viewMenu
+
+    def __setupPigletMenu(self):
+        """
+        Setup the piglet menu
+        """
+        pigletMenu = wx.Menu()
+        return pigletMenu
+
+    def __setupHelpMenu(self):
+        """
+        Setup the help menu
+        """
+        helpMenu = wx.Menu()
+        
+        seekHelp = wx.MenuItem(helpMenu, wx.ID_ANY, '&Help', 'Seek help')
+        requestFeature = wx.MenuItem(helpMenu, wx.ID_ANY, 'Request feature', 'Request feature')
+        reportBug = wx.MenuItem(helpMenu, wx.ID_ANY, 'Report bug', 'Submit bug')
+        about = wx.MenuItem(helpMenu, wx.ID_ANY, '&About', 'About')
+
+        helpMenu.AppendItem(seekHelp)
+        helpMenu.AppendSeparator()
+        helpMenu.AppendItem(requestFeature)
+        helpMenu.AppendItem(reportBug)
+        helpMenu.AppendSeparator()
+        helpMenu.AppendItem(about)
+
+        self.parentFrame.Bind(wx.EVT_MENU, self.__onAbout, id=about.GetId())
+        
+        return helpMenu
+
+    ##--------------------------------------------------------------------------
+    ## Bind functions
+    ##--------------------------------------------------------------------------
+
+    def __onAbout(self, event):
+        """
+        Populates an about dialog and displays it
+        """
+        information = wx.AboutDialogInfo()
+
+        information.SetName('Open Definition :: Warpig Code Environment')
+        information.SetVersion('0.02 - Alpha Public')
+        information.SetWebSite('http://www.opendefinition.com')
+        information.SetCopyright('Open Definition(C)2009')
+        information.AddDeveloper('Roger C.B. Johnsen')
+        information.SetDescription('A simple but yet powerfull code evironment written in Python.')
+
+        wx.AboutBox( information )
+
+    def __onExit(self, event):
+        """
+        On exit event
+        """
+        dialog = wx.MessageDialog(
+                            None,
+                            'Are you sure to want to quit?',
+                            'Question',
+                            wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION
+                        )
+
+        status = dialog.ShowModal()
+
+        if( status == wx.ID_YES ):
+            dialog.Destroy()
+            self.parentFrame.Close()
+        else:
+            dialog.Destroy()
+
+    def __onPreferences(self, event):
+        """
+        Display the preference menu
+        """
+        preferences = WpPreferences()
+	preferences.ShowModal()
+
+    def __onNewProject( self, event ):
+        """
+        Display new project dialog window
+        """
+  	window = WpNewProject(self.parentFrame.mainpanel.leftsplit)
+	window.ShowModal()
+	window.Destroy()
+
+    def __onOpenProject( self, event ):
+        """
+        Display open project dialog window
+        """
+        window = WpOpenProject(self.parentFrame.mainpanel.leftsplit)
+        window.ShowModal()
