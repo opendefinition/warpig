b842acb7382c9db6623235b25cc3cf9e7672f45c
Roger C.B. Johnsen <johnsen@opendefinition.com>
2010-01-15 22:30:56 +0100

    Fixed indentation. Closes #18

--- source/warpig.py
+++ source/warpig.py
@@ -20,46 +20,46 @@ from system.WpConfigLoader import WpConfigLoader
 from gui.Keybindings import Keybindings
 
 class WpWarPig( wx.App ):
-	"""
-	Main class for creating frame
-	"""
-	
-	def OnInit(self):
-		configloaded = False
-		try:
-			configuration = WpConfigLoader()
-			configuration.LoadConfig()
-			configloaded = True
-		except sqlite3.OperationalError:
-			print "Warpig System Error: missing configuration database\nplease run 'warpig.py --setup'"
+    """
+    Main class for creating frame
+    """
 
-		if configloaded:
-                        ## Load keybindings
-                        keybinding = Keybindings(self)
-                        keybinding.init()
+    def OnInit(self):
+        configloaded = False
+        try:
+            configuration = WpConfigLoader()
+            configuration.LoadConfig()
+            configloaded = True
+        except sqlite3.OperationalError:
+            print "Warpig System Error: missing configuration database\nplease run 'warpig.py --setup'"
 
-			frame = WpAuiMainFrame( None )
-			frame.Show()
-			self.SetTopWindow(frame)
-		
-		return True
+        if configloaded:
+            ## Load keybindings
+            keybinding = Keybindings(self)
+            keybinding.init()
+
+            frame = WpAuiMainFrame( None )
+            frame.Show()
+            self.SetTopWindow(frame)
+
+        return True
 
 def main(argv):
-	"""
-	Warpig booter
-	"""
-	if len( argv ) > 1:
-		if argv[1] == '--update':
-			# Update engine
-			print 'Running update engine (not implemented)'
-		if argv[1] == '--setup':
-			RunSetup() # a setup is basically a full reset
-		if argv[1] == '--reset':
-			RunSetup() # a reset is basically a new installation			
-	else:
-		# Boot
-		app = WpWarPig( 0 )
-		app.MainLoop()
+    """
+    Warpig booter
+    """
+    if len( argv ) > 1:
+        if argv[1] == '--update':
+            # Update engine
+            print 'Running update engine (not implemented)'
+        if argv[1] == '--setup':
+            RunSetup() # a setup is basically a full reset
+        if argv[1] == '--reset':
+            RunSetup() # a reset is basically a new installation
+    else:
+        # Boot
+        app = WpWarPig( 0 )
+        app.MainLoop()
         
 if __name__ == "__main__":
-	sys.exit( main( sys.argv ) )
+    sys.exit( main( sys.argv ) )
