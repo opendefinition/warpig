263260dfcfb5fcb84baab87bbd48c3f0925dec04
Roger C.B. Johnsen <johnsen@opendefinition.com>
2010-01-15 23:12:41 +0100

    Fixed indendation. Closes #27

--- source/gui/WpOpenProject.py
+++ source/gui/WpOpenProject.py
@@ -23,119 +23,119 @@ from wx.lib.pubsub import Publisher as pub
 
 
 class WpOpenProject( wx.Dialog ):
-	def __init__(self):
-            wx.Dialog.__init__(self, None, CONST_WIDGET_PROJECT_OPEN, 'Open Project', size=(500, 300))
-            self.Center()
-            self.Setup()
-                
-        def Setup(self):
-            mainsizer = wx.FlexGridSizer(2,1, vgap=5, hgap=5)
-            mainsizer.AddMany(
-                [
-                    (self.ProjectList(), 1, wx.EXPAND | wx.ALL, 5),
-                    (self.Buttons(), 1, wx.EXPAND | wx.ALL, 15)
-                ]
-            )
+    def __init__(self):
+        wx.Dialog.__init__(self, None, CONST_WIDGET_PROJECT_OPEN, 'Open Project', size=(500, 300))
+        self.Center()
+        self.Setup()
 
-            mainsizer.AddGrowableRow( 0 )
-            mainsizer.AddGrowableCol( 0 )
+    def Setup(self):
+        mainsizer = wx.FlexGridSizer(2,1, vgap=5, hgap=5)
+        mainsizer.AddMany(
+            [
+                (self.ProjectList(), 1, wx.EXPAND | wx.ALL, 5),
+                (self.Buttons(), 1, wx.EXPAND | wx.ALL, 15)
+            ]
+        )
 
-            self.SetSizer(mainsizer, wx.EXPAND)
+        mainsizer.AddGrowableRow( 0 )
+        mainsizer.AddGrowableCol( 0 )
 
-        def ProjectList(self):
-            panel = wx.Panel(self, CONST_WIDGET_PROJECT_PANEL)
-            panelsizer = wx.BoxSizer(wx.VERTICAL)
-            
-            self.projectlist = wx.ListCtrl(
-                                    panel, CONST_WIDGET_PROJECT_LIST,
-                                    style=wx.BORDER_SUNKEN | wx.LC_REPORT | wx.LC_VRULES | wx.LC_HRULES | wx.LC_SINGLE_SEL | wx.LC_NO_HEADER
-                                )
+        self.SetSizer(mainsizer, wx.EXPAND)
 
-            self.projectlist.InsertColumn(0, 'Project')
-            self.projectlist.SetColumnWidth(0, 1000)
+    def ProjectList(self):
+        panel = wx.Panel(self, CONST_WIDGET_PROJECT_PANEL)
+        panelsizer = wx.BoxSizer(wx.VERTICAL)
 
-            ## Populate the list
-            for project in self.__GetProjectList():
-                item = wx.ListItem()
-                item.SetText(project['title'])
-                item.SetData(project['id'])
+        self.projectlist = wx.ListCtrl(
+                                panel, CONST_WIDGET_PROJECT_LIST,
+                                style=wx.BORDER_SUNKEN | wx.LC_REPORT | wx.LC_VRULES | wx.LC_HRULES | wx.LC_SINGLE_SEL | wx.LC_NO_HEADER
+                            )
 
-                self.projectlist.InsertItem(item)
+        self.projectlist.InsertColumn(0, 'Project')
+        self.projectlist.SetColumnWidth(0, 1000)
 
-            panelsizer.Add(self.projectlist, 1, wx.EXPAND)
-            panel.SetSizer(panelsizer, wx.EXPAND)
+        ## Populate the list
+        for project in self.__GetProjectList():
+            item = wx.ListItem()
+            item.SetText(project['title'])
+            item.SetData(project['id'])
 
-            return panel
+            self.projectlist.InsertItem(item)
 
-        def __GetProjectList(self):
+        panelsizer.Add(self.projectlist, 1, wx.EXPAND)
+        panel.SetSizer(panelsizer, wx.EXPAND)
+
+        return panel
+
+    def __GetProjectList(self):
+        db = WpDatabaseAPI()
+        return db.GetProjectList()
+
+    def Buttons(self):
+        panel = wx.Panel(self, CONST_WIDGET_PROJECT_LIST_PANEL)
+        panelsizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        openbutton = buttons.ThemedGenButton(panel, CONST_WIDGET_PROJECT_BUTTON_OPEN, 'Open')
+        cancelbutton = buttons.ThemedGenButton(panel, CONST_WIDGET_PROJECT_BUTTON_CANCEL, 'Cancel')
+        deletebutton = buttons.ThemedGenButton(panel, CONST_WIDGET_PROJECT_BUTTON_DELETE, 'Delete')
+
+        panelsizer.Add(deletebutton, 0, wx.RIGHT, 2)
+        panelsizer.Add(cancelbutton)
+        panelsizer.Add(openbutton, 1, wx.EXPAND | wx.RIGHT, 2)
+
+        panel.SetSizer(panelsizer)
+
+        self.Bind(wx.EVT_BUTTON, self.onOpen, id=CONST_WIDGET_PROJECT_BUTTON_OPEN)
+        self.Bind(wx.EVT_BUTTON, self.onCancel, id=CONST_WIDGET_PROJECT_BUTTON_CANCEL)
+        self.Bind(wx.EVT_BUTTON, self.onDelete, id=CONST_WIDGET_PROJECT_BUTTON_DELETE)
+
+        return panel
+
+    ##----------------------------------------------------------------------
+    ## Bindings
+    ##----------------------------------------------------------------------
+
+    def onOpen(self, event):
+        projectid = self.__getItemDataForSelectedElement()
+
+        if projectid != None:
             db = WpDatabaseAPI()
-            return db.GetProjectList()
+            project = db.GetProject(projectid)
 
-        def Buttons(self):
-            panel = wx.Panel(self, CONST_WIDGET_PROJECT_LIST_PANEL)
-            panelsizer = wx.BoxSizer(wx.HORIZONTAL)
+            # Open tab
+            pub.sendMessage('notebook.opensavedtabs', projectid)
 
-            openbutton = buttons.ThemedGenButton(panel, CONST_WIDGET_PROJECT_BUTTON_OPEN, 'Open')
-            cancelbutton = buttons.ThemedGenButton(panel, CONST_WIDGET_PROJECT_BUTTON_CANCEL, 'Cancel')
-            deletebutton = buttons.ThemedGenButton(panel, CONST_WIDGET_PROJECT_BUTTON_DELETE, 'Delete')
+            # Populate the tree
+            pub.sendMessage('projecttree.populate', project)
 
-            panelsizer.Add(deletebutton, 0, wx.RIGHT, 2)
-            panelsizer.Add(cancelbutton)
-            panelsizer.Add(openbutton, 1, wx.EXPAND | wx.RIGHT, 2)
-
-            panel.SetSizer(panelsizer)
-
-            self.Bind(wx.EVT_BUTTON, self.onOpen, id=CONST_WIDGET_PROJECT_BUTTON_OPEN)
-            self.Bind(wx.EVT_BUTTON, self.onCancel, id=CONST_WIDGET_PROJECT_BUTTON_CANCEL)
-            self.Bind(wx.EVT_BUTTON, self.onDelete, id=CONST_WIDGET_PROJECT_BUTTON_DELETE)
-
-            return panel
-
-        ##----------------------------------------------------------------------
-        ## Bindings
-        ##----------------------------------------------------------------------
-
-        def onOpen(self, event):
-            projectid = self.__getItemDataForSelectedElement()
-
-            if projectid != None:
-                db = WpDatabaseAPI()
-                project = db.GetProject(projectid)
-               
-                # Open tab
-                pub.sendMessage('notebook.opensavedtabs', projectid)
-
-                # Populate the tree
-                pub.sendMessage('projecttree.populate', project)
-
-                self.Destroy()
-
-        def onCancel(self, event):
             self.Destroy()
 
-        def onDelete(self, event):
-            projectid = self.__getItemDataForSelectedElement()
+    def onCancel(self, event):
+        self.Destroy()
 
-            if projectid != None:
-                db = WpDatabaseAPI()
-                db.DeleteProject(projectid)
+    def onDelete(self, event):
+        projectid = self.__getItemDataForSelectedElement()
 
-                self.projectlist.DeleteItem(self.__getSelectedItem())
+        if projectid != None:
+            db = WpDatabaseAPI()
+            db.DeleteProject(projectid)
+
+            self.projectlist.DeleteItem(self.__getSelectedItem())
 
 
-        def __getItemDataForSelectedElement(self):
-            """
-            Get itemdata for current selected project
-            Return None if none selected
-            """
-            try:
-                data = int(self.projectlist.GetItemData(self.projectlist.GetFirstSelected()))
-                return data
-            except:
-                return None
+    def __getItemDataForSelectedElement(self):
+        """
+        Get itemdata for current selected project
+        Return None if none selected
+        """
+        try:
+            data = int(self.projectlist.GetItemData(self.projectlist.GetFirstSelected()))
+            return data
+        except:
+            return None
 
-        def __getSelectedItem(self):
-            """
-            Get selected project
-            """
-            return self.projectlist.GetFirstSelected()
+    def __getSelectedItem(self):
+        """
+        Get selected project
+        """
+        return self.projectlist.GetFirstSelected()
