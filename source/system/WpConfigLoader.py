a1c14fd452b7fce7341dc34ef676edf08cb19ae8
Roger C.B. Johnsen <johnsen@opendefinition.com>
2010-01-17 09:19:31 +0100

    Fixed indentation. Closes #31

--- source/system/WpConfigLoader.py
+++ source/system/WpConfigLoader.py
@@ -15,14 +15,14 @@ from system.WpDatabaseAPI import WpDatabaseAPI
 from system.WpConfigSystem import WpConfigSystem
 
 class WpConfigLoader:
-	def LoadConfig( self ):
-		db = WpDatabaseAPI()
-		
-		storage = WpConfigSystem()
-		storage.settings = {}
-		
-		registrysettings = db.LoadRegistry()
+    def LoadConfig( self ):
+        db = WpDatabaseAPI()
 
-		for config in registrysettings:
-			setting = config[2] + "-" + config[0]
-			storage.settings[setting] = config[1]
+        storage = WpConfigSystem()
+        storage.settings = {}
+
+        registrysettings = db.LoadRegistry()
+
+        for config in registrysettings:
+            setting = config[2] + "-" + config[0]
+            storage.settings[setting] = config[1]
